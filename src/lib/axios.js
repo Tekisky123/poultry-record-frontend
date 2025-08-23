31c3eeee146f23c231445802feec64e827b2812b
Tauhid <tekiskyhr@gmail.com>
2025-08-23 13:00:46 +0530

    Vercel Baseurl Added

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8888/api';
+// const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8888/api';
+const apiBaseUrl = "https://poultry-record-backend.vercel.app/api";
 
 const api = axios.create({
 	baseURL: apiBaseUrl,
