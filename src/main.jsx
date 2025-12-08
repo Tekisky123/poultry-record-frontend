805f992c9cc119eba8561bda6832c368e0b8d01f
Tohid4641 <dev.tauhid@gmail.com>
2025-12-09 01:26:23 +0530

    service worker changes

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("/service-worker.js", { type: "module" })
       .then(reg => console.log("SW registered:", reg))
       .catch(err => console.log("SW registration failed:", err));
   });
