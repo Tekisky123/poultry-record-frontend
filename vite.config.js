ab4410c6614ecd21e07e7f0e226c7d183ef1a085
Tohid4641 <dev.tauhid@gmail.com>
2025-12-09 01:32:59 +0530

    vite changes for service worker

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
+    includeAssets: ["favicon.ico", "logo192.png", "logo512.png"],
+    manifest: {
+      name: "RCC AND TRADING COMPANY",
+      short_name: "RCC",
+      theme_color: "#000000",
+      background_color: "#000000",
+      display: "standalone",
+      start_url: "/",
+      icons: [
+        { src: "logo192.png", sizes: "192x192", type: "image/png" },
+        { src: "logo512.png", sizes: "512x512", type: "image/png" }
+      ]
+    }
+  })],
   css: {
     postcss: './postcss.config.js'
   }
